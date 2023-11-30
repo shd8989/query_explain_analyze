491dca36c0a3075a9c5e77ba833d57192016ae9e
hyundong-seo <terranpush@naver.com>
2023-11-30 12:47:34 +0900

    추가 - 엑셀 파일을 이용한 멀티 쿼리 실행 기능 추가

--- backend/app.js
+++ backend/app.js
@@ -62,6 +62,29 @@ app.post(api_context + '/single-query', (req) => {
             client.query(insertQuery, [query, '', 'Fail', 'Error code: ' + e.code + ":: Hint: " + e.hint])
             .catch((e) => console.error(e.stack));
         })
-        .finally(() => {client.end();});
     });
+    pool.on('end', function() {client.end();});
+});
+
+app.post(api_context + '/multi-query', (req) => {
+    const {queries} = req.body.data;
+    pool.connect(function(err, client) {
+        if(err) {
+            console.log('connection error', err);
+        }
+        for(var i=0; i<Object.values(queries).length; i++) {
+            const insertQuery = "INSERT INTO result_functional_test (query, return_data, rst, error_msg, insert_dt) VALUES ($1, $2, $3, $4, now())";
+            let query = Object.values(Object.values(queries)[i])[0];
+            client.query(query)
+            .then((res) => {
+                client.query(insertQuery, [query, Object.values(res.rows[0])[0], 'Success', ''])
+                .catch((e) => console.error(e.stack));
+            })
+            .catch((e) => {
+                client.query(insertQuery, [query, '', 'Fail', 'Error code: ' + e.code + ":: Hint: " + e.hint])
+                .catch((e) => console.error(e.stack));
+            })
+        }
+    });
+    pool.on('end', function() {client.end();});
 });
--- frontend/src/common/Contents.jsx
+++ frontend/src/common/Contents.jsx
@@ -1,8 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import axios from 'axios';
+import * as XLSX from 'xlsx';
 
 const Contents = () => {
   const [text, setText] = useState('');
+  const [uploadFile, setUploadFile] = useState(null);
 
   const execQuery = async () => {
     setText(text => text);
@@ -14,12 +16,46 @@ const Contents = () => {
     }
   };
 
+  const fileUpload = async () => {
+    setUploadFile(uploadFile => uploadFile);
+    if(uploadFile) {
+      await axios.post('/api/v1/multi-query', {
+        data: uploadFile
+      })
+      .then(response => {console.log(response);});
+    }
+  };
+
+  const fileChange = useCallback(async (e) => {
+    if (e) {
+      const file = e[0];
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: "array", bookVBA: true });
+
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const queries = XLSX.utils.sheet_to_json(sheet);
+
+        setUploadFile({queries});
+      };
+
+      reader.readAsArrayBuffer(file);
+    }
+  }, []);
+
   function onChange(e) {
     setText(e.target.value);
-  }
+  };
 
   useEffect(() => {
     execQuery();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    fileUpload();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -31,32 +67,33 @@ const Contents = () => {
             <div className="col">
               <h1 className="mt-4">File upload with query</h1>
               <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" defaultValue="0">
-                <option value="0">Open this select menu</option>
+                <option value="0">Database Connection Information</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
               </select>
-              <input className="form-control" type="file" id="formFile" />
+              <input className="form-control" type="file" id="formFile" onChange={(e) => fileChange(e.target.files)} accept=".xlsx, .xls" />
+              <div className="warning_font">(Warning) This only reads data from the first sheet.</div>
               <div className="col-xl-3 col-md-6">
                 <div className="card bg-primary text-white mb-4">
-                  <div className="card-body">Success Card</div>
+                  <div className="card-body" onClick={fileUpload}>Execute Multi Queries</div>
                 </div>
               </div>
             </div>
             <div className="col">
               <h1 className="mt-4">Execute a query</h1>
               <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" defaultValue="0">
-                <option value="0">Open this select menu</option>
+                <option value="0">Database Connection Information</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
               </select>
               <div className="form-floating">
-                <textarea className="form-control" onChange={onChange} id="floatingTextarea"></textarea>
+                <textarea className="form-control" onChange={(e) => onChange(e)} id="floatingTextarea"></textarea>
               </div>
               <div className="col-xl-3 col-md-6">
                 <div className="card bg-primary text-white mb-4">
-                  <div className="card-body" onClick={execQuery}>Execute</div>
+                  <div className="card-body" onClick={execQuery}>Execute Single Query</div>
                 </div>
               </div>
             </div>
