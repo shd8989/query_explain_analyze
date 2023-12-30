60e04b0d5a746d91490493d3d409787d63496919
hyundong-seo <terranpush@naver.com>
2023-12-30 17:54:35 +0900

    수정 - #21 queryplan text에 depth별로 출력되도록 수정

--- frontend/src/user/QueryPlanText.jsx
+++ frontend/src/user/QueryPlanText.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const QueryPlanText = ({resultFirst, resultSecond}) => {
+  const regex = /\]/;
   return (
     <>
       <main>
@@ -9,14 +10,36 @@ const QueryPlanText = ({resultFirst, resultSecond}) => {
             <div className="col">
               <div className="text-nowrap">
                 <p className="queryplan_align">
-                  {resultFirst}
+                  {
+                    resultFirst.split('\n')
+                    .map(line => {
+                      if(line.substring(0, 1) !== ('P' || 'E')) {
+                        return line.replace(line.substring(0, Number(line.lastIndexOf(line.match(regex)))+1), '') + '\n'
+                      } else if(line.substring(0, 1) === 'P') {
+                        return line + '\n';
+                      } else {
+                        return line;
+                      }
+                    })
+                  }
                 </p>
               </div>
             </div>
             <div className="col">
               <div className="text-nowrap">
                 <p className="queryplan_align">
-                  {resultSecond}
+                  {
+                    resultSecond.split('\n')
+                    .map(line => {
+                      if(line.substring(0, 1) !== ('P' || 'E')) {
+                        return line.replace(line.substring(0, Number(line.lastIndexOf(line.match(regex)))+1), '') + '\n'
+                      } else if(line.substring(0, 1) === 'P') {
+                        return line + '\n';
+                      } else {
+                        return line;
+                      }
+                    })
+                  }
                 </p>
               </div>
             </div>
