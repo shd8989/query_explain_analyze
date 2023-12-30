aee367a3342dd59461b5328707ab8b8fa137a372
hyundong-seo <terranpush@naver.com>
2023-12-30 09:56:11 +0900

    추가 - #8 라이브러리 샘플 적용

--- frontend/src/user/QueryPlanView.jsx
+++ frontend/src/user/QueryPlanView.jsx
@@ -1,26 +1,158 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'react-flow-renderer';
 
-const QueryPlanView = () => {
-    return (
-      <>
-        <main>
-          <div className="container-fluid px-4">
-            <div className="row">
-              <div className="col">
-                <div className="text-nowrap">
-                  Query Plan(View)
-                </div>
-              </div>
-              <div className="col">
-                <div className="text-nowrap">
-                  Query Plan(View)
-                </div>
-              </div>
+const initialNodes = [
+  {
+    id: 'horizontal-1',
+    sourcePosition: 'right',
+    type: 'input',
+    data: { label: 'Input' },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: 'horizontal-2',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'A Node' },
+    position: { x: 200, y: 0 },
+  },
+  {
+    id: 'horizontal-3',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'Node 3' },
+    position: { x: 200, y: 80 },
+  },
+  {
+    id: 'horizontal-4',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'Node 4' },
+    position: { x: 400, y: 0 },
+  },
+  {
+    id: 'horizontal-5',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'Node 5' },
+    position: { x: 400, y: 80 },
+  },
+  {
+    id: 'horizontal-6',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'Node 6' },
+    position: { x: 400, y: 160 },
+  },
+  {
+    id: 'horizontal-7',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'Node 7' },
+    position: { x: 600, y: 80 },
+  },
+  {
+    id: 'horizontal-8',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'Node 8' },
+    position: { x: 600, y: 160 },
+  },
+];
+
+const initialEdges = [
+  {
+    id: 'horizontal-e1-2',
+    source: 'horizontal-1',
+    type: 'smoothstep',
+    target: 'horizontal-2',
+    animated: true,
+  },
+  {
+    id: 'horizontal-e1-3',
+    source: 'horizontal-1',
+    type: 'smoothstep',
+    target: 'horizontal-3',
+    animated: true,
+  },
+  {
+    id: 'horizontal-e1-4',
+    source: 'horizontal-2',
+    type: 'smoothstep',
+    target: 'horizontal-4',
+    label: 'edge label',
+  },
+  {
+    id: 'horizontal-e3-5',
+    source: 'horizontal-3',
+    type: 'smoothstep',
+    target: 'horizontal-5',
+    animated: true,
+  },
+  {
+    id: 'horizontal-e3-6',
+    source: 'horizontal-3',
+    type: 'smoothstep',
+    target: 'horizontal-6',
+    animated: true,
+  },
+  {
+    id: 'horizontal-e5-7',
+    source: 'horizontal-5',
+    type: 'smoothstep',
+    target: 'horizontal-7',
+    animated: true,
+  },
+  {
+    id: 'horizontal-e6-8',
+    source: 'horizontal-6',
+    type: 'smoothstep',
+    target: 'horizontal-8',
+    animated: true,
+  },
+];
+
+const QueryPlanView = (resultFirst, resultSecond) => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback((params) => setEdges((els) => addEdge(params, els)), []);
+
+  console.log(resultFirst, resultSecond);
+  return (
+    <>
+      <main>
+        <div className="container-fluid px-4">
+          <div className="row min500">
+            <div className="col">
+              <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+                attributionPosition="bottom-left"
+              ></ReactFlow>
+            </div>
+            <div className="col">
+              <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+                attributionPosition="bottom-left"
+              ></ReactFlow>
             </div>
           </div>
-        </main>
-      </>
-    );
+          <div>
+          
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default React.memo(QueryPlanView);
