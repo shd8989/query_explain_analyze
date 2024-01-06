0c347fbf1419bad2da8fb88fab181e167d5b4c7b
hyundong-seo <terranpush@naver.com>
2024-01-06 12:20:30 +0900

    수정 - logo 연결링크 수정

--- frontend/src/common/Header.jsx
+++ frontend/src/common/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
       <>
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         {/* Navbar Brand*/}
-        <a className="navbar-brand ps-3" href="index.html">Analyze Query</a>
+        <a className="navbar-brand ps-3" href="dbconn_list">Analyze Query</a>
         {/* Sidebar Toggle*/}
         <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i className="fas fa-bars" /></button>
         {/* Navbar Search*/}
